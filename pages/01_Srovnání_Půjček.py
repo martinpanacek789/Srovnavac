26cd183bd45dca29cc19965b4ecd4e600c747805
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-06-19 13:44:58 +0200

    Moved FAQ to separate page, added nav button module for it

--- pages/01_Srovnání_Půjček.py
+++ pages/01_Srovnání_Půjček.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 
 from modules.loan import Loan
 from modules.comparator import Comparator
+from modules.change_page import nav_page
 
 @st.cache_data
 def load_loan_data():
@@ -114,35 +115,6 @@ if st.session_state.calculated:
         # Display the output table
         st.table(loan.payment_plan)
 
-st.header("FAQ:")
+    if st.button('Více informací'):
+        nav_page("FAQ")
 
-st.subheader("Na co si mohu půjčku vzít?")
-st.write("Půjčky jsou nejčastěji na pokrytí výdajů na **studium v zahraničí**, **vzdělávací kurzy**, "
-         "**vybavení domácnosti** či **koupi auta**. "
-         "Jedná se o spotřebitelské neúčelové půjčky pro financování osobních potřeb a cílů.")
-
-st.subheader("Proč věřit tomuto srovnávači?")
-st.write("Mnoho jiných srovnávačů finančních produktů může mít nasmlouvané produkty, "
-         "které posouvají na přední příčky při vyhledávání. "
-         "Společnost Zlatá koruna je nezávislá platforma, "
-         "která Vám vytvoří žebříček půjček dle Vaších preferencí na míru.")
-
-st.subheader("Jaký je rozdíl mezi bankovní a nebankovní půjčkou?")
-st.write("Nebankovní půjčka je poskytována institucemi, které nejsou součástí bankovního sektoru, "
-         "na rozdíl od bankovních půjček. Nebankovní půjčky jsou často dostupnější a rychlejší k získání, "
-         "ovšem mohou mít")
-
-st.subheader("Co znamenají ocenění Zlaté koruny u finančních produktů?")
-st.write("Soutěž Zlatá koruna oceňuje nejlepší finanční produkty na českém trhu. "
-         "Rozdává zlatou, stříbrnou a bronzovou korunu dle mínění hodnotitelů. "
-         "Navíc ještě uděluje zelenou korunu značící udržitelnost produktu.")
-
-st.subheader("Jaký úrok dostanu?")
-st.write("Ve srovnávači je uvedena minimální úroková sazba. Přesné číslo není možné personalizovat, "
-         "neboť jej banka uděluje na základě charakteru žadatele a úvěru "
-         "(bonita, úvěrové riziko, doba splatnosti apod.). Některé půjčky mohou mít i fixní úrokovou sazbu.")
-
-st.subheader("Co zvážit při výběru půjčky?")
-st.write("Při zvažování půjčky si promyslete, zdali je to skutečně nezbytné. "
-         "Zvažte svoji schopnost půjčku dále splácet,"
-         " aby se negativně neohrozily Vaše budoucí finanční možnosti. Nezapomeňte si pečlivě pročíst podmínky.")
---
+++ pages/99_FAQ.py
@@ -0,0 +1,35 @@
+# 99_FAQ.py
+import streamlit as st
+
+st.title("FAQ")
+
+st.subheader("Na co si mohu půjčku vzít?")
+st.write("Půjčky jsou nejčastěji na pokrytí výdajů na **studium v zahraničí**, **vzdělávací kurzy**, "
+         "**vybavení domácnosti** či **koupi auta**. "
+         "Jedná se o spotřebitelské neúčelové půjčky pro financování osobních potřeb a cílů.")
+
+st.subheader("Proč věřit tomuto srovnávači?")
+st.write("Mnoho jiných srovnávačů finančních produktů může mít nasmlouvané produkty, "
+         "které posouvají na přední příčky při vyhledávání. "
+         "Společnost Zlatá koruna je nezávislá platforma, "
+         "která Vám vytvoří žebříček půjček dle Vaších preferencí na míru.")
+
+st.subheader("Jaký je rozdíl mezi bankovní a nebankovní půjčkou?")
+st.write("Nebankovní půjčka je poskytována institucemi, které nejsou součástí bankovního sektoru, "
+         "na rozdíl od bankovních půjček. Nebankovní půjčky jsou často dostupnější a rychlejší k získání, "
+         "ovšem mohou mít")
+
+st.subheader("Co znamenají ocenění Zlaté koruny u finančních produktů?")
+st.write("Soutěž Zlatá koruna oceňuje nejlepší finanční produkty na českém trhu. "
+         "Rozdává zlatou, stříbrnou a bronzovou korunu dle mínění hodnotitelů. "
+         "Navíc ještě uděluje zelenou korunu značící udržitelnost produktu.")
+
+st.subheader("Jaký úrok dostanu?")
+st.write("Ve srovnávači je uvedena minimální úroková sazba. Přesné číslo není možné personalizovat, "
+         "neboť jej banka uděluje na základě charakteru žadatele a úvěru "
+         "(bonita, úvěrové riziko, doba splatnosti apod.). Některé půjčky mohou mít i fixní úrokovou sazbu.")
+
+st.subheader("Co zvážit při výběru půjčky?")
+st.write("Při zvažování půjčky si promyslete, zdali je to skutečně nezbytné. "
+         "Zvažte svoji schopnost půjčku dále splácet,"
+         " aby se negativně neohrozily Vaše budoucí finanční možnosti. Nezapomeňte si pečlivě pročíst podmínky.")
