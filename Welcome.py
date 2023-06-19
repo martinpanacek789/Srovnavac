b02595e9c80fc62cc8dc5261ee42f7ee1bb05f65
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-06-19 17:03:33 +0200

    Test select tiles

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
+from streamlit_option_menu import option_menu
+
 st.title("Hello World")
 
 st.write("Title page of the app")
 
 st.write("Some text here")
+
+
+selected2 = option_menu(None, ['Vlastní parametry', "Bydlení", "Auto", "Studium", "Zahraniční pobyt"],
+                        icons=['gear', 'house', 'car-front', "book", "airplane"],
+                        menu_icon="cast", default_index=0, orientation="horizontal")
+
+st.write(selected2)
