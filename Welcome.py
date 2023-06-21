aafa7a1fca7b323b673fe8ec6f33ae2ae08817ad
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-06-21 20:50:56 +0200

    Richer Data, fixed minor issues

--- Welcome.py
+++ Welcome.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
+import pandas as pd
+
 st.title("Srovnávač půjček")
 
 st.write("Rozhodnutí ohledně financí a půjček může být pro mladé lidi obtížné. "
          "S naším nekomerčním porovnávačem spotřebitelských půjček "
          "získáte přehled o nabídkách od různých poskytovatelů."
          "Díky personalizovanému vyhledávání najdete půjčku Vám přesně na míru!")
+
+
