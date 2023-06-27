1430cd3f01de4e30d109c8bf8cfba383e876b72c
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-06-27 22:30:38 +0200

    minor changes

--- Úvod.py
+++ Úvod.py
@@ -9,12 +9,12 @@ col1, col2 = st.columns(2)
 with col1:
     if st.button("Srovnání půjček", key="srovnani_pujcek", use_container_width=True):
         switch_page("Srovnání půjček")
-         # st.write("Srovnání půjček")
+        # st.write("Srovnání půjček")
 
 with col2:
     if st.button("Textové vyhledávání", key="textove_vyhledavani", use_container_width=True):
         switch_page("Textové vyhledávání")
-         # st.write("Textové vyhledávání")
+        # st.write("Textové vyhledávání")
 
 st.header("O projektu")
 
@@ -22,7 +22,7 @@ st.markdown("Jsme skupina studentů z FIS VŠE, kteři se v rámci datového pro
             "pro mladé lidi ve spolupráci s neziskovým projektem Zlatá Koruna.\n")
 
 st.markdown("Aplikace je založena na moderních technologiích, obsahuje textové vyhledávání využívající umělou "
-            "inteligenci a další funkce, které umožnují uživateli snadno a rychle najít odpovídaící půjčku.\n")
+            "inteligenci a další funkce, které umožnují uživateli snadno a rychle najít odpovídající půjčku.\n")
 
 st.markdown("Děkujeme, že jste navštívili naši aplikaci a doufáme, že se Vám bude líbit.\n"
             "Tým studentů datové analytiky FIS VŠE")
