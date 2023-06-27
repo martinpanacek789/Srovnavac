9dca0e6a2dae34446c6fd16fe060ce452a34ce45
Martin Panacek <79920088+martinpanacek789@users.noreply.github.com>
2023-06-27 21:50:00 +0200

    Change select tiles location, minor changes, try link

--- Welcome.py
+++ Welcome.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
-import pandas as pd
-
 st.title("Srovnávač půjček")
 
-st.write("Rozhodnutí ohledně financí a půjček může být pro mladé lidi obtížné. "
-         "S naším nekomerčním porovnávačem spotřebitelských půjček "
-         "získáte přehled o nabídkách od různých poskytovatelů."
-         "Díky personalizovanému vyhledávání najdete půjčku Vám přesně na míru!")
+st.write("check out this [link](https://martinpanacek789-srovnavac-welcome-clc5f6.streamlit.app/Srovnání_Půjček)")
+
+
+st.header("O projektu")
+
+st.markdown("Jsme skupina studentů z FIS VŠE, kteři se v rámci datového projektu věnovali vytvoření srovnávače půjček "
+            "pro mladé lidi ve spolupráci s neziskovým projektem Zlatá Koruna.\n")
+
+st.markdown("Aplikace je založena na moderních technologiích, obsahuje textové vyhledávání využívající umělou "
+            "inteligenci a další funkce, které umožnují uživateli snadno a rychle najít odpovídaící půjčku.\n")
+
+st.markdown("Děkujeme, že jste navštívili naši aplikaci a doufáme, že se Vám bude líbit.\n"
+            "Tým studentů datové analytiky FIS VŠE")
+
+
+
 
 
